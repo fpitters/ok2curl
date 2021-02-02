5738c7f9c688f8b2942a9cb77e83eceeb4112933
Fernando Pitters <fernando.pitters@tv2.no>
2021-02-02 21:05:32 +0100

    Initial implementation of ok2curl

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ok2curl
 Okhttp logs to curl
+
+I learnt too late about [Ok2Curl](https://github.com/mrmike/Ok2Curl) 
+
+# Usage
+Pass a log file:
+`python ok2curl.py -f sample.txt`
+
+or pipe it in:
+`cat sample.txt | python ok2curl.py`
+
+Output
+```
+>>> Curl command is:
+curl -i -X GET -H "Authorization: Bearer TOKEN" -H "custom-header: XXX" -H "User-Agent: Dalvik/2.1.0 (Linux; U; Android 11; KB2003 Build/RP1A.201005.001)" -H "Host: ai.sumo.tv2.no" -H "Connection: Keep-Alive" -H "If-Modified-Since: Mon, 01 Feb 2021 12" "http://fpitters.com/api/v1/dummy"
+```
---
+++ ok2curl.py
@@ -0,0 +1,78 @@
+import sys
+import argparse
+import re
+import fileinput
+from dataclasses import dataclass, field
+from typing import List, ClassVar
+
+@dataclass
+class Header:
+    """Header model"""
+    key: str
+    value: str
+
+@dataclass
+class Request:
+    """Request model"""
+    method: str
+    url: str
+    is_data: bool
+    data: str = None
+    headers: list = field(default_factory=list)
+
+    EXCLUDED_HEADERS: ClassVar[list] = ['Accept-Encoding']
+
+    def curl(self):
+        h = ' '.join([f'-H "{header.key}: {header.value}"' for header in self.headers if header.key not in Request.EXCLUDED_HEADERS])
+        if not self.is_data:
+            return f'curl -i -X {self.method} {h} "{self.url}"'
+        else:
+            return f'curl -i -X {self.method} --data \'{self.data}\' {h} "{self.url}"'
+
+def parseFile(file):
+    pattern_json = '^[{\[].*[}\]]$'
+    for line in fileinput.input(file):
+        logs = line.split('D/OkHttp: ')
+        if len(logs) > 1:
+            log = logs[1].strip()
+            if log.startswith('-->'):
+                url_params = log.split()
+                command = url_params[1]
+                if command == 'END':
+                    if request == None:
+                        print('Invalid end of request reached without any request')
+                    else:
+                        print('>>> Curl command is:')
+                        print(request.curl())
+                        request = None
+                else:
+                    is_data = command == 'POST' or command == 'PUT'
+                    request = Request(command, url_params[2], is_data) 
+            else:
+                if not request:
+                    # print('Invalid header or data without any request')
+                    pass
+                else:
+                    # it's either a header or json body
+                    if request.is_data and re.search(pattern_json, log):
+                        request.data = log
+                    else:
+                        header_str = log.split(':')
+                        header = Header(header_str[0].strip(), header_str[1].strip())
+                        request.headers.append(header)
+    if request:
+        print('>>> Incomplete request:')
+        print(request.curl())
+        request = None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parse OkHttp logs')
+    parser.add_argument('-f', '--file', action='store',
+            help='file with the OkHttp logs to be parsed')
+    args = parser.parse_args()
+    
+    if(args.file):
+        parseFile(args.file)
+    else:
+        parseFile(None)
+
